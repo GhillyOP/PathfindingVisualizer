672194315ebf6acfc0e4c2e9cb13b07d813b4d6d
philoche33 <philbolduc200@gmail.com>
2020-02-04 19:57:06 -0500

    MinHeap decrease key

--- src/data-structures/min-heap.js
+++ src/data-structures/min-heap.js
@@ -4,111 +4,121 @@
  * The Node.distance will be used to compare and sort the nodes in the heap
  */
 export default class MinHeap {
+  constructor() {
+    this.heap = [];
+    this.map = new Map();
 
-    constructor() {
-        this.heap = [];
-        this.map = new Map();
+    this.test();
+  }
 
-        this.test()
+  getMin() {
+    return this.heap[0];
+  }
 
+  insert(node) {
+    this.heap.push(node);
+
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      let element = this.heap[index];
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.heap[parentIndex];
+
+      if (parent.distance <= element.distance) break;
+      this.heap[index] = parent;
+      this.heap[parentIndex] = element;
+      index = parentIndex;
     }
 
-    getMin() {
-        return this.heap[0];
+    //set node heapIndex in map with it's index as key
+    this.map.set(`${node.row}:${node.col}`, index);
+  }
+
+  remove() {
+    let smallest = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.sinkDown(0);
+    return smallest;
+  }
+
+  sinkDown(index) {
+    let left = 2 * index + 1;
+    let right = 2 * index + 2;
+    let smallest = index;
+    const length = this.heap.length;
+
+    // if left child is greater than parent
+    if (
+      left < length &&
+      this.heap[left].distance < this.heap[smallest].distance
+    ) {
+      smallest = left;
     }
-
-    insert(node) {
-        this.heap.push(node);
-
-        let index = this.heap.length - 1;
-
-        while (index > 0) {
-            let element = this.heap[index]
-            let parentIndex = Math.floor((index - 1) / 2)
-            let parent = this.heap[parentIndex]
-
-            if (parent.distance <= element.distance) break
-            this.heap[index] = parent;
-            this.heap[parentIndex] = element;
-            index = parentIndex
-        }
+    // if right child is greater than parent
+    if (
+      right < length &&
+      this.heap[right].distance < this.heap[smallest].distance
+    ) {
+      smallest = right;
     }
-
-    remove() {
-        let smallest = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.sinkDown(0)
-        return smallest;
+    // swap
+    if (smallest !== index) {
+      [this.heap[smallest], this.heap[index]] = [
+        this.heap[index],
+        this.heap[smallest]
+      ];
+      this.sinkDown(smallest);
     }
+  }
 
+  changeDistance(node, distance) {
+    // get node in map and set distance
+    const heapIndex = this.map.get(`${node.row}:${node.col}`);
+    this.heap[heapIndex].distance = distance;
 
-    sinkDown(index) {
-        let left = 2 * index + 1
-        let right = 2 * index + 2
-        let smallest = index;
-        const length = this.heap.length
+    // heapify tree
+    while (
+      heapIndex > 0 &&
+      this.heap[heapIndex].distance < this.heap[(heapIndex - 1) / 2].dist
+    ) {
+        // swap the node with it's parent
+        let temp = this.heap[heapIndex];
+        this.heap[heapIndex] = this.heap[(heapIndex - 1) / 2];
+        this.heap[(heapIndex - 1) / 2] = temp;
 
-
-        // if left child is greater than parent
-        if (left < length && this.heap[left].distance < this.heap[smallest].distance) {
-            smallest = left
-        }
-        // if right child is greater than parent
-        if (right < length && this.heap[right].distance < this.heap[smallest].distance) {
-            smallest = right
-        }
-        // swap
-        if (smallest !== index) {
-            [this.heap[smallest], this.heap[index]] = [this.heap[index], this.heap[smallest]]
-            this.sinkDown(smallest)
-        }
+        // set the index to the parent index
+        heapIndex = (heapIndex - 1) / 2;
     }
+  }
 
-    changeDistance(key, distance) {
-        const node = this.map.get(key);
+  test() {
+    const node1 = {
+      distance: 1
+    };
+    const node2 = {
+      distance: 2
+    };
+    const node3 = {
+      distance: 8
+    };
+    const node4 = {
+      distance: 2
+    };
+    const node5 = {
+      distance: 5
+    };
 
-        node.distance = distance;
+    this.insert(node1);
+    this.insert(node2);
+    this.insert(node3);
+    this.insert(node4);
+    this.insert(node5);
 
-        // heapify tree
-    }
-
-    test() {
-
-        const node1 = {
-            distance: 1
-        }
-        const node2 = {
-            distance: 2
-        }
-        const node3 = {
-            distance: 8
-        }
-        const node4 = {
-            distance: 2
-        }
-        const node5 = {
-            distance: 5
-        }
-
-
-        this.insert(node1)
-        this.insert(node2)
-        this.insert(node3)
-        this.insert(node4)
-        this.insert(node5)
-
-        console.log(this.remove());
-        console.log(this.remove());
-        console.log(this.remove());
-        console.log(this.remove());
-        console.log(this.remove());
-
-
-
-
-
-    }
-
-
-
+    console.log(this.remove());
+    console.log(this.remove());
+    console.log(this.remove());
+    console.log(this.remove());
+    console.log(this.remove());
+  }
 }
